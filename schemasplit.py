a55027707b7995bb96e84456221cca7451a695ad
Jakob Schlyter <jakob@kirei.se>
2023-10-20 08:20:41 +0200

    log

--- schemasplit.py
+++ schemasplit.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import logging
 from copy import deepcopy
 from pathlib import Path
 
@@ -15,8 +16,11 @@ def main():
     parser.add_argument("schema", metavar="filename")
     args = parser.parse_args()
 
+    logging.basicConfig(level=logging.INFO)
+
     with open(args.schema) as fp:
         schema = yaml.safe_load(fp)
+        logging.info("Read %s", args.schema)
 
     subschemas = schema.get("anyOf", [])
 
@@ -25,20 +29,23 @@ def main():
         subschemas_names.append(subschema["$ref"].split("/")[-1])
 
     for subschema in subschemas:
-        name = subschema["$ref"].split("/")[-1]
+        subschema_name = subschema["$ref"].split("/")[-1]
+
+        logging.info("Processing %s", subschema_name)
 
         schema2 = deepcopy(schema)
 
-        schema2["properties"].update(schema["components"]["schemas"][name])
+        schema2["properties"].update(schema["components"]["schemas"][subschema_name])
 
         del schema2["anyOf"]
         for n in subschemas_names:
             del schema2["components"]["schemas"][n]
 
-        output = Path(args.schema).stem + f"-{name}.json"
+        output = Path(args.schema).stem + f"-{subschema_name}.json"
 
         with open(output, "wt") as fp:
             json.dump(schema2, fp)
+            logging.info("Wrote %s", output)
 
 
 if __name__ == "__main__":
